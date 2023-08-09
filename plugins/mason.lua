c1335d53d759a70b7ee3aa6dd218c0f63324e6d5
Daniele Gargano <daniele.gargano.office@gmail.com>
2023-08-09 10:48:46 +0200

    Adds plugins, dap, linters

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,10 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "pyright",
+        "bashls",
+        "gopls",
       })
     end,
   },
@@ -18,7 +21,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
+        "prettier",
+        "black",
+        "gofumpt"
         -- "stylua",
       })
     end,
@@ -30,6 +35,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "python",
+        "delve",
       })
     end,
   },
